d21a50f300500f8f06414422de3cc738cb39006c
Guilherme Rocha <guilhermeazevedo032@gmail.com>
2025-10-17 20:56:38 -0300

    Tirando AI commands de alguns arquivos.

---
+++ Migrations/20251017235316_RemoveAIColumnsFromCall.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoPIM4Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAIColumnsFromCall : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
